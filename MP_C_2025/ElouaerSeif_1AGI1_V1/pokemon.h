96b8ae537cee390a08dae2936100eaf36d9f5074
Seif Elouaer <74378447+SeifElouaer@users.noreply.github.com>
2026-01-23 00:16:45 +0100

    Add files via upload

---
+++ MP_C_2025/ElouaerSeif_1AGI1_V1/pokemon.h
@@ -0,0 +1,25 @@
+#ifndef POKEMON_H_INCLUDED
+#define POKEMON_H_INCLUDED
+
+#include "headers.h"
+
+typedef struct pokemon{
+    int id;
+    char nom[20];
+    float prix;
+    char type[];
+}pokemon;
+
+/********Declaration fonctions POKEMON************/
+ETUDIANT saisirEtud();
+void AfficherEtudiant(ETUDIANT e);
+void modifier_etudiant(ETUDIANT *etd);
+
+int AjouterEtudiant(ETUDIANT TabEtud[],int last_index, int taille);
+void afficherTabEtudiant(ETUDIANT TabEtud[],int n);
+
+int rechercherId(ETUDIANT TabEtud[],int n);
+int rechercherEtudiantNom(ETUDIANT TabEtud[], int n, char nom[]);
+void supprimer_etudiant(ETUDIANT TabEtud[],int *N, int pos);
+
+#endif // POKEMON_H_INCLUDED
